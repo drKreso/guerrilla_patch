f998a9dfaeb28d66eb6bd36f6de04e27d34ad4c4
drKreso <kresimir.bojcic@gmail.com>
2012-03-31 00:20:43 +0200

    array extension for sum

--- lib/guerrilla_patch/kernel.rb
+++ lib/guerrilla_patch/kernel.rb
@@ -1,44 +1,56 @@
 module Kernel
-   def let(name, &block)
-     define_method(name, &block)
-   end
+  def let(name, &block)
+    define_method(name, &block)
+  end
 
-   def let_self(name, &block)
-     define_singleton_method(name, &block)
-   end
+  def let_self(name, &block)
+    define_singleton_method(name, &block)
+  end
 
-   def when_present(item, &block)
-     if block_given?
-       item.nil? ? '' : block.call(item)
-     else
-       item
-     end
-   end
+  def when_present(item, &block)
+    if block_given?
+      item.nil? ? '' : block.call(item)
+    else
+      item
+    end
+  end
 
-   def consists_of
-     PoorsManStringBuilder.new.tap { |builder| yield(builder) }.result
-   end
+  def consists_of
+    PoorsManStringBuilder.new.tap { |builder| yield(builder) }.result
+  end
 
-   class PoorsManStringBuilder
-     attr_reader :result
+  class PoorsManStringBuilder
+    attr_reader :result
 
-     def initialize
-       @result = ''
-     end
+    def initialize
+      @result = ''
+    end
 
-     def add(item)
-       @result << item.to_s
-     end
-     alias :always :add
+    def add(item)
+      @result << item.to_s
+    end
+    alias :always :add
 
-     def when_present(item, &block)
-       @result << Kernel.when_present(item, &block).to_s
-     end
+    def when_present(item, &block)
+      @result << Kernel.when_present(item, &block).to_s
+    end
 
-     def when(item, &block)
-       return unless item
-       @result << Kernel.when_present(item, &block).to_s
-     end
-   end
+    def when(item, &block)
+      return unless item
+      @result << Kernel.when_present(item, &block).to_s
+    end
+  end
+
+
+  #add sum to array, with or without name
+  class Array
+    def sum
+      self.reduce(0, :+)
+    end
+
+    def sum(name)
+      self.map(&name).reduce(0, :+)
+    end
+  end
 
 end
