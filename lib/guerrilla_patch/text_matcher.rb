d7a18f40dc89a789f4e8505e73b45c165477c03c
drKreso <kresimir.bojcic@gmail.com>
2016-03-06 08:03:18 +0100

    Build fix

--- lib/guerrilla_patch/text_matcher.rb
+++ lib/guerrilla_patch/text_matcher.rb
@@ -5,8 +5,8 @@ class TextMatcher
     source.each_pair { |key,value| source[key] = value.gsub(' ', '').downcase }
     target.each_pair { |key,value| target[key] = value.gsub(' ', '').downcase }
 
-    mapped_source = source.each_with_index.map { |key_value,index| [index.to_s * key_value[1].size] }.flatten(1).join('')
-    mapped_target = target.each_with_index.map { |key_value,index| [index.to_s * key_value[1].size] }.flatten(1).join('')
+    mapped_source = source.each_with_index.map { |key_value,index| [index] * key_value[1].size }.flatten(1)
+    mapped_target = target.each_with_index.map { |key_value,index| [index] * key_value[1].size }.flatten(1)
 
     source_clean_chars = source.each_value.map {|v| v}.join('')
     target_clean_chars = target.each_value.map {|v| v}.join('')
@@ -19,7 +19,7 @@ class TextMatcher
         if source_clean_chars[source_index] == nil
           target_index += 1
         elsif source_clean_chars[source_index] != target_clean_chars[target_index]
-          mapped_source.insert(source_index, 'M')
+          mapped_source.insert(source_index, '-')
           source_index += 1
         else
           source_index += 1
@@ -33,8 +33,16 @@ class TextMatcher
     end
 
     (0...mapped_target.size).each do |index|
-      result[mapped_target[index].to_i] ||= []
-      result[mapped_target[index].to_i] << [mapped_source[index].to_i]
+      if index == "-"
+        #nothing should happen
+      else
+        result[mapped_target[index]] ||= []
+        if mapped_source[index] == "-"
+          #nothing should happen
+        else
+          result[mapped_target[index]] << [mapped_source[index]]
+        end
+      end
     end
     result.each_pair { |key, value| result[key] = value.flatten.uniq }
 
